7062d0a1cfcf857d2ed9b500fc3eb6b30c1b2c84
abhinavshukl12-alt <abhinavshukl12@gmail.com>
2026-05-29 12:57:49 +0530

    script.js

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ function closePopup() {
 document.getElementById("popup")
 .style.display = "none";
 }
+document.addEventListener(
+"mousemove",
+
+e => {
+
+    const dot =
+    document.createElement("div");
+
+    dot.className =
+    "trail";
+
+    dot.style.left =
+    e.pageX + "px";
+
+    dot.style.top =
+    e.pageY + "px";
+
+    document.body.appendChild(dot);
+
+    setTimeout(() => {
+
+        dot.remove();
+
+    }, 500);
+});
