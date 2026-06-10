7ba0decea8be4620b1d38c4dbb6d5794ebf7f207
abhinavshukl12-alt <abhinavshukl12@gmail.com>
2026-06-10 18:50:39 +0530

    Update omega.js

--- omega.js
+++ omega.js
@@ -1,22 +1,30 @@
 const omega = document.createElement("div");
 
 omega.innerHTML = "🟡";
-omega.id = "omega";
+
+omega.style.position = "fixed";
+omega.style.left = "200px";
+omega.style.bottom = "100px";
+omega.style.fontSize = "80px";
+omega.style.zIndex = "999999";
+omega.style.pointerEvents = "none";
 
 document.body.appendChild(omega);
 
-let x = 100;
+let x = 200;
 let dir = 1;
 
 function animate() {
 
-    x += dir * 2;
+    x += dir * 3;
 
-    if (x > window.innerWidth - 50)
+    if (x > window.innerWidth - 100) {
         dir = -1;
+    }
 
-    if (x < 0)
+    if (x < 0) {
         dir = 1;
+    }
 
     omega.style.left = x + "px";
 
